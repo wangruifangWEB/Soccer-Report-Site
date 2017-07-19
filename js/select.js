8a9588a9da4534779d70dff6b1b24ddf198cd414
Chuancong Gao <chuanconggao@users.noreply.github.com>
2017-07-18 17:10:19 -0700

    Add tag for select with groups

---
+++ js/select.js
@@ -0,0 +1,23 @@
+function updateTag(selectObj) {
+    var tag = $(selectObj).find("option:selected").parent("optgroup").attr("label");
+    $(selectObj).prev().html(tag);
+}
+
+function improveSelects() {
+    $("select").not("[multiple]").filter(function() {
+        return $(this).attr("data-key");
+    }).each(function() {
+        $(this).before(
+            `
+                <span class="label label-xs label-default"></span>
+            `
+        );
+
+        setTimeout(() => {
+            $(this).change(function() {
+                updateTag(this);
+            });
+            updateTag(this);
+        }, 400);
+    });
+}
